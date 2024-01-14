322dbca6bc3e6af18c8f5c97d1e9e5a0e0dfb0df
Joe Freeman <joefreeman8@gmail.com>
2024-01-14 14:46:18 +0000

    Populating the show page with more pokemon info.

--- src/views/PokemonShowView.vue
+++ src/views/PokemonShowView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import { RouterLink, useRoute } from 'vue-router'
 
 const route = useRoute()
 const pokemon = ref({})
@@ -22,6 +22,18 @@ onMounted(fetchSinglePokemon)
 
 <template>
   <main>
-    <h1>{{ pokemon.name }} Info Page</h1>
+    <h1>#{{ pokemon.number }} - {{ pokemon.name }}</h1>
+    <img :src="pokemon.image" :alt="pokemon.name" />
+    <p>Type: 
+      <span v-for="type, index in pokemon.type" :key="index">
+        <strong>
+          {{ type[0].toUpperCase() + type.slice(1, type.length) }}{{ index < pokemon.type.length - 1 ? ', ' : '' }}
+        </strong>
+      </span>
+    </p>
+    <p>Habitat: {{ pokemon.habitat }}</p>
+    <p>Pokedex: {{ pokemon.pokedexEntry }}</p>
+    <br />
+    <RouterLink :to="'/pokemon'">Back To All Pokemon</RouterLink>
   </main>
 </template>
